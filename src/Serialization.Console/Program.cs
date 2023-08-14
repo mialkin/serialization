96ba6fd665fdf85243017c6a700167381a3b23bb
Aleksei Mialkin <anmyalkin@gmail.com>
2023-08-14 16:53:58 +0700

    Run program

---
+++ src/Serialization.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serialization.Common;
+using Serialization.Newtonsoft.Serializers;
+using Serialization.SystemTextJson.Serializers;
+
+var car = CarFactory.Build();
+
+var defaultJsonSerializer = new DefaultJsonSerializer();
+var camelCaseJsonSerializer = new CamelCaseJsonSerializer();
+var redisJsonSerializer = new RedisJsonSerializer();
+var newtonsoftJsonSerializer = new NewtonsoftJsonSerializer();
+
+Console.WriteLine(defaultJsonSerializer.Serialize(car));
+Console.WriteLine(camelCaseJsonSerializer.Serialize(car));
+Console.WriteLine(redisJsonSerializer.Serialize(car));
+Console.WriteLine(newtonsoftJsonSerializer.Serialize(car));
+
+/*
+Output:
+
+{"Make":"Honda","Model":0,"YearOfProduction":2020,"Colors":{"Roof":"White","Wheels":"Dark grey"}}
+{"make":"Honda","model":0,"yearOfProduction":2020,"colors":{"Roof":"White","Wheels":"Dark grey"}}
+{"make":"Honda","model":"HondaCivic","yearOfProduction":2020,"colors":{"roof":"White","wheels":"Dark grey"}}
+{"Make":"Honda","Model":0,"YearOfProduction":2020,"Colors":{"Roof":"White","Wheels":"Dark grey"}}
+
+*/
